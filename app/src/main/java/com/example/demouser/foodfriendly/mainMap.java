4049c5cd0b65034d66f0d21db26db8662c43d03c
mondobot <mateipavaluca@yahoo.com>
2014-07-31 17:59:10 +0100

    renamed mainMap.java

--- app/src/main/java/com/example/demouser/foodfriendly/mainMap.java
+++ app/src/main/java/com/example/demouser/foodfriendly/mainMap.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class mainMap extends Activity {
+public class MainMap extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
