41244b53490621fb8e9ad8f439ab2ed02f040080
mondobot <mateipavaluca@yahoo.com>
2014-08-01 10:31:49 +0100

    What.

--- app/src/main/java/com/example/demouser/foodfriendly/map/DetailsFragment.java
+++ app/src/main/java/com/example/demouser/foodfriendly/map/DetailsFragment.java
@@ -2,15 +2,12 @@ package com.example.demouser.foodfriendly.map;
 
 
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.demouser.foodfriendly.FilterFragment;
-import com.example.demouser.foodfriendly.FragmentCallback;
 import com.example.demouser.foodfriendly.R;
 
 /**
@@ -31,7 +28,7 @@ public class DetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.details_layout, container, false);
+        return inflater.inflate(R.layout.details_item, container, false);
     }
 
 
